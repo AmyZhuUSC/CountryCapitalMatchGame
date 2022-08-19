36414e4b7ca20de36b5fa6f05d5614445daa86d1
AmyZhuUSC <111546473+AmyZhuUSC@users.noreply.github.com>
2022-08-19 11:40:01 -0500

    basic files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculater from "./Calculater"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Calculater
+        </p>
+        <Calculater />
+      </header>
+    </div>
+  );
+}
+
+export default App;
